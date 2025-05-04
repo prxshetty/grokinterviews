c03ddc3e61a3ef844c6af45cb0df18f379eb6a2e
pranam-gf <pranam@goodfin.com>
2025-05-04 11:32:43 -0400

    markdown content removed and related apis

--- src/app/components/content/index.ts
+++ src/app/components/content/index.ts
@@ -1,2 +1,4 @@
-export * from './MarkdownHeaderTree';
+// export * from './MarkdownHeaderTree'; // Component deleted
 // export { default as QuestionContentDisplay } from './QuestionContentDisplay'; // Removed
+
+// This directory might be empty or contain other unrelated components now.
