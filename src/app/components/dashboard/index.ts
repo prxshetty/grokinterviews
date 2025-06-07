2236f61bbb881a35c124b4923abd44e75035d2ee
Pranam <prxshetty@gmail.com>
2025-06-07 15:33:29 -0400

    time filter removed, metric cards alignment

--- src/app/components/dashboard/index.ts
+++ src/app/components/dashboard/index.ts
@@ -2,4 +2,5 @@ export { default as OverviewWidget } from './OverviewWidget';
 export { default as DomainCompletionWidget } from './DomainCompletionWidget';
 export { default as RecentActivityWidget } from './RecentActivityWidget';
 export { default as UserStatsWidget } from './UserStatsWidget';
-export { UserActivityChart } from './UserActivityChart'; 
+export { UserActivityChart } from './UserActivityChart';
+export { MetricCards } from './MetricCards'; 
