946c55f0fdb6c25274a03eb75322d66989ab5aeb
Pranam <prxshetty@gmail.com>
2025-07-01 15:02:30 -0400

    added difficulty pagination support

--- src/components/topics-ui/ContentDisplay.tsx
+++ src/components/topics-ui/ContentDisplay.tsx
@@ -263,11 +263,13 @@ export default function ContentDisplay({
             </div>
             
             {totalPages > 1 && (
-              <Pagination
-                currentPage={currentPage}
-                totalPages={totalPages}
-                onPageChange={onPageChange}
-              />
+              <div className="mt-6 flex justify-center">
+                <Pagination
+                  currentPage={currentPage}
+                  totalPages={totalPages}
+                  onPageChange={onPageChange}
+                />
+              </div>
             )}
           </>
         ) : null}
