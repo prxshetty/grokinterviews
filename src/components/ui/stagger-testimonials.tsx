09528871b56ecf7dd6b84900287454b6e51a2775
Pranam <prxshetty@gmail.com>
2025-07-08 16:10:31 -0400

    lazy loading, removed link with Image

--- src/components/ui/stagger-testimonials.tsx
+++ src/components/ui/stagger-testimonials.tsx
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import { useScrollAnimation } from '@/hooks/ui';
+import Image from 'next/image';
 
 const SQRT_5000 = Math.sqrt(5000);
 
@@ -177,9 +178,11 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({
           height: 2
         }}
       />
-      <img
+      <Image
         src={testimonial.imgSrc}
         alt={`${testimonial.by.split(',')[0]}`}
+        width={48}
+        height={56}
         className="mb-4 h-14 w-12 bg-muted object-cover object-top"
         style={{
           boxShadow: "3px 3px 0px hsl(var(--background))"
