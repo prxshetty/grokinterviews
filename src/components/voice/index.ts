debaeed7bb65f5d346688130778b66a93debc7f7
Pranam <prxshetty@gmail.com>
2025-07-20 17:05:08 -0400

    bug fixes, Memos wrapped

--- src/components/voice/index.ts
+++ src/components/voice/index.ts
@@ -6,6 +6,3 @@ export * from './phone'
 
 // Shared components
 export * from './shared'
-
-// Keep InterviewModeSelector in main directory for now (will be integrated inline)
-export { default as InterviewModeSelector } from './InterviewModeSelector'
